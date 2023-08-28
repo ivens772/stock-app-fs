5b68fa6077838efe5e7dc44f2af5db22c28ec28b
ivens772 <ivens772@gmail.com>
2023-08-28 12:26:17 -0600

    modificando package json

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 app.get('/',(req,res)=>{
- res.send("hola soy una nueva app con nodemon")
+ res.send("hola soy una nueva app con nodemon en git")
 });
 
 app.listen(PORT, ()=>{
